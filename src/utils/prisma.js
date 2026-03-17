a8d791aeee9dadd6ec2eaa9ed132adcb46b527af
khoihthe181332 <hatuankhoi2004@gmail.com>
2026-03-18 01:23:32 +0700

    fix: remove unsigned types and sync supabase

--- src/utils/prisma.js
+++ src/utils/prisma.js
@@ -1,15 +1,7 @@
 const { PrismaClient } = require("../../generated/prisma");
-const { PrismaMariaDb } = require("@prisma/adapter-mariadb");
-const { databaseConfig } = require("@/config");
+const { PrismaPg } = require("@prisma/adapter-pg");
 
-const adapter = new PrismaMariaDb({
-  host: databaseConfig.host,
-  user: databaseConfig.user,
-  password: databaseConfig.password,
-  database: databaseConfig.database,
-  port: databaseConfig.port,
-  allowPublicKeyRetrieval: true,
-});
+const adapter = new PrismaPg(process.env.DATABASE_URL);
 
 const prisma = new PrismaClient({ adapter });
 
